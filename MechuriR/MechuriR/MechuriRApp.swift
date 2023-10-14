96b7425cf7d8d2b5314495689b0216b0cb1f6eac
Hminchae <hminchae@gmail.com>
2023-10-14 15:16:42 +0900

    build loginView

---
+++ MechuriR/MechuriR/MechuriRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechuriRApp.swift
+//  MechuriR
+//
+//  Created by 황민채 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct MechuriRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
