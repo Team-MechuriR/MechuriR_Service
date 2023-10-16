e9cf61e2ab4238ab422cd758ce0c680eaf8e9f23
a3a2a <a3a2a@naver.com>
2023-10-16 21:58:35 +0900

    'Modify...'

--- MechuriR_Flutter/lib/main.dart
+++ MechuriR_Flutter/lib/main.dart
@@ -10,14 +10,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MechuriR',  // AppName
+      themeMode: ThemeMode.light,
       theme: ThemeData(
         brightness: Brightness.light, // LightMode
-        primaryColor: Color(0xffF3EFE4), // BackgroundColor
-        backgroundColor: Color(0xffF3EFE4),
+        //primaryColor: Color(0xffF3EFE4), // 강조색
+        scaffoldBackgroundColor: Color(0xffF3EFE4), // 앱 배경색
+
+        // AppBar
+        appBarTheme: const AppBarTheme(
+          //shadowColor: Colors.black,
+          backgroundColor: Color(0xffF3EFE4), // 상단바 배경색
+        ),
+
+        // FloatingActionButton
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xffD4C4AB) // 플로팅 버튼 배경색
+        ),
+
+        textTheme: const TextTheme(
+
+        )
+        //Define the default font family.
+        //fontFamily: '',
         //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        //useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -45,8 +63,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        centerTitle: true,  // 가운데 정렬
+        title: Image.asset('logo.png', height: 30,) // 로고 이미지
       ),
       body: Center(
         child: Column(
@@ -62,11 +80,19 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: SizedBox(
+        width: 60,
+        height: 60,
+        child: FloatingActionButton( // 플로팅 버튼
+          onPressed: _incrementCounter,
+          tooltip: 'New',
+          child: SizedBox(
+            width: 60,
+            height: 60,
+            child : Icon(Icons.add,),
+          )
+        ),
+      ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
