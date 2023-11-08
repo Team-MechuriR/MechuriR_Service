79594cd870d74b562d23f751757076b99bfbcef8
a3a2a <a3a2a@naver.com>
2023-11-08 21:58:21 +0900

    Add [mypage.dart] -ing

--- MechuriR_Flutter/lib/mypage.dart
+++ MechuriR_Flutter/lib/mypage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mechuri_r/mypage.dart';
-import 'package:mechuri_r/providers/counts.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light, // LightMode
         scaffoldBackgroundColor: Color(0xffF3EFE4), // 앱 배경색
 
-
         // AppBar
         appBarTheme: const AppBarTheme(
           //shadowColor: Colors.black,
@@ -29,51 +28,65 @@ class MyApp extends StatelessWidget {
             backgroundColor: Color(0xffD4C4AB) // 플로팅 버튼 배경색
         ),
       ),
-      home: const MainPage(),);
+      home: const MyPage(),);
   }
 }
 
 
 
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,  // 가운데 정렬
-          title: Image.asset('images/logo.png', height: 35,), // 로고 이미지
-          foregroundColor: Color(0xff878787),
-        ),
-        body: ListView(
-          controller: ScrollController(),
-          children: [
-            Container(width: double.infinity, height: 40, color: Color(0xffD4C4AB),
-              child: Container(margin: EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('[user name]의 일기장', style: TextStyle(color: Color(0xff403831)),),]
+      appBar: AppBar(
+        centerTitle: true,  // 가운데 정렬
+        title: Image.asset('images/logo.png', height: 35,), // 로고 이미지
+        foregroundColor: Color(0xff878787),
+      ),
+      body: ListView(
+        controller: ScrollController(),
+        children: [
+          Column(
+            children: [
+              Container(width: double.infinity, height: 40, color: Color(0xffD4C4AB),
+                child: Container(margin: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment : MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                                icon: Icon(Icons.close, color: Color(0xff403831),),
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                            ),
+                            Text('마이페이지', style: TextStyle(color: Color(0xff403831), fontWeight: FontWeight.bold),),
+                            IconButton(
+                              icon: Icon(Icons.done, color: Color(0xff403831),),
+                              onPressed: (){
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      ]
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
-        floatingActionButton: SizedBox(
-          width: 60,
-          height: 60,
-          child: FloatingActionButton( // 플로팅 버튼
-              onPressed: (){},
-              tooltip: 'New',
-              child: SizedBox(
-                width: 60,
-                height: 60,
-                child : Icon(Icons.add,),
-              )
+
+            ],
           ),
-        ),
+
+        ],
+      ),
       endDrawer: Container(
         width: 250,
         child: Drawer(
@@ -117,10 +130,7 @@ class MainPage extends StatelessWidget {
                       leading: Icon(Icons.person, color: Colors.white,),
                       title: Text('마이페이지', style: TextStyle(color: Colors.white),),
                       onTap: (){
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const MyPage())
-                        );
+                        Navigator.pop(context);
                       },
                     ),
                     ListTile(
@@ -177,10 +187,10 @@ class MainPage extends StatelessWidget {
                     ),
                     Row(
                       children: [
-                       TextButton(
-                           onPressed: (){},
-                           child: Image.asset('images/insta_logo.png')
-                       ),
+                        TextButton(
+                            onPressed: (){},
+                            child: Image.asset('images/insta_logo.png')
+                        ),
                         TextButton(
                             onPressed: (){},
                             child: Image.asset('images/facebook_logo.png')
@@ -201,6 +211,39 @@ class MainPage extends StatelessWidget {
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+
+
+PickedFile _imageFile;
+final ImagePicker _picker = ImagePicker();
+
+Widget imageProfile(){
+  return Center(
+    child: Stack(
+      children: [<Widget>[
+        CircleAvatar(
+          radius: 170,
+          backgroundImage: _imageFile == null
+            ? AssetImage('assets/profile1.png'):FileImage(File(_imageFile.path)),
+        ),
+        Positioned(
+          bottom: 20, right: 20,
+          child: InkWell(
+            onTap: (){
+              // 클릭시 모달 팝업을 띄워준다.
+              showModalBottomSheet(context: context, builder: ((builder)=>bottomSheet()));
+            },
+            child: Icon(
+              Icons.camera_alt,
+              color: Colors.white,
+              size:40,
+            ),
+          ),
+        )
+      ]],
+    ),
+  )
 }
 
 
