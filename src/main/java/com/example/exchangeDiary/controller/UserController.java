f8eba5cb11b3ae5e720dfaae62e9b90f242720d6
Jiseon-Lee <43613836+Jiseon-Lee@users.noreply.github.com>
2023-12-03 10:11:52 +0900

    Update UserController.java

--- src/main/java/com/example/exchangeDiary/controller/UserController.java
+++ src/main/java/com/example/exchangeDiary/controller/UserController.java
@@ -8,19 +8,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.exchangeDiary.dto.*;
-import com.example.exchangeDiary.service.UserService;
+import com.example.exchangeDiary.serviceImpl.UserServiceImpl;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
-	private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+	@Autowired
+	private UserServiceImpl userService;
     
     // 로그인
-    @PostMapping("/user/login")
+    @PostMapping("/login")
     public ResponseEntity<UserInfoDto> loginCheck(@RequestBody UserInfoDto dto, HttpSession session) {
     	System.out.println("UserController - loginCheck");
     	UserInfoDto userInfo = userService.loginCheck(dto, session);
@@ -32,7 +33,7 @@ public class UserController {
     }
 
     // 회원 가입
-    @PostMapping("/user/signup")
+    @PostMapping("/signup")
     public ResponseEntity<Void> registerUser(UserSignupReqDto dto) {
     	System.out.println("UserController - registerUser");
     	int result = userService.registerUser(dto);
